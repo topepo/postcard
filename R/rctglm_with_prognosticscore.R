eca56f5bba04328318b487b4732777eb9df30da6
Mathias Lerbech Jeppesen <mathiasljeppesen@outlook.com>
2024-12-13 22:20:48 -0800

    feat: Added function to fit rctglm with prognostic score

---
+++ R/rctglm_with_prognosticscore.R
@@ -0,0 +1,76 @@
+#' Title
+#'
+#' @param formula
+#' @param family
+#' @param data
+#' @param group_indicator
+#' @param group_allocation_prob
+#' @param estimand_fun
+#' @param estimand_fun_deriv0
+#' @param estimand_fun_deriv1
+#' @param ...
+#' @param data_hist
+#' @param n_folds
+#' @param learners
+#' @param prog_formula
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#' rctglm_with_prognosticscore(formula = Y ~ ., group_indicator = A,
+#' data = dat_norm, family = gaussian(), estimand_fun = "ate",
+#' data_hist = dat_norm)
+rctglm_with_prognosticscore <- function(formula,
+                                     family,
+                                     data,
+                                     group_indicator,
+                                     group_allocation_prob = 1/2,
+                                     estimand_fun = "ate",
+                                     estimand_fun_deriv0 = NULL, estimand_fun_deriv1 = NULL,
+                                     ...,
+                                     data_hist,
+                                     n_folds = 5,
+                                     learners = default_learners(),
+                                     prog_formula = formula) {
+
+  if (is.character(formula)) formula <- formula(formula)
+  if (is.character(prog_formula)) prog_formula <- formula(prog_formula)
+
+  group_indicator <- rlang::enquo(group_indicator)
+  named_args <- as.list(environment())
+  extra_glm_args <- list(...)
+
+  data_from_formula <- do.call(
+    model.frame,
+    c(list(formula = prog_formula, data = data),
+      extra_glm_args
+    )
+  )
+
+  cv_folds <- rsample::vfold_cv(data_hist, v = n_folds)
+  lrnr <- cv_folds %>%
+    get_best_learner(learners = learners,
+                     formula = prog_formula) %>%
+    fit(data_hist)
+
+  lrnr_pred <- predict(lrnr, data_from_formula) %>%
+    dplyr::pull(.pred)
+  data %<>% mutate(prog = lrnr_pred)
+
+  formula_with_prognosticscore <- paste0(deparse(formula), " + prog")
+
+  rctglm_with_prognosticscore <- rctglm(
+    formula = formula_with_prognosticscore,
+    family = family,
+    data = data,
+    group_indicator = group_indicator,
+    group_allocation_prob = group_allocation_prob,
+    estimand_fun = estimand_fun,
+    estimand_fun_deriv0 = estimand_fun_deriv0,
+    estimand_fun_deriv1 = estimand_fun_deriv1,
+    ...
+  )
+
+  return(rctglm_with_prognosticscore)
+}
---
+++ R/superlearner.R
@@ -0,0 +1,95 @@
+# Find a prognostic score from hist data
+
+default_learners <- function() {
+  list(
+    mars = list(
+      model = parsnip::mars(
+        mode = "regression", prod_degree = 3) %>%
+        parsnip::set_engine("earth"),
+      grid = NULL
+    ),
+    lm = list(
+      model = parsnip::linear_reg() %>%
+        parsnip::set_engine("lm"),
+      grid = NULL
+    ),
+    gbt = list(
+      model = parsnip::boost_tree(
+        mode = "regression",
+        trees = parsnip::tune("trees"),
+        tree_depth = parsnip::tune("tree_depth"),
+        learn_rate = 0.1
+      ) %>%
+        parsnip::set_engine("xgboost"),
+      grid = purrr::cross_df(list(
+        trees = seq.int(25, 500, by=25),
+        tree_depth = c(3)
+      ))
+    )
+  )
+}
+
+# set up the pre-processing of the work flow
+get_preproc_names <- function(wf) {
+  wf %>%
+    dplyr::pull(wflow_id) %>%
+    stringr::str_split("_") %>%
+    purrr::map_chr(~.[1]) %>%
+    unique()
+}
+
+# function to add learners with the pre-processing
+add_learners <- function(preproc, learners) {
+  wf <- workflowsets::workflow_set(
+    preproc = preproc,
+    models = learners %>%
+      map(~.$model)
+  )
+  for (learner_name in names(learners)){
+    for (preproc_name in get_preproc_names(wf)) {
+      wf %<>%
+        workflowsets::option_add(
+          id = stringr::str_c(preproc_name, "_", learner_name),
+          grid = learners[[learner_name]]$grid
+        )
+    }
+  }
+  wf
+}
+
+# K fold cross validation with recipe -------------------------------------
+get_best_learner <- function(
+    resamples,
+    learners = default_learners(),
+    formula,
+    verbose = T
+) {
+
+  wfs <- add_learners(preproc = list(noProg = formula),
+                      learners = learners)
+
+  fit_learners <- wfs %>%
+    workflowsets::workflow_map(
+      resamples = resamples,
+      metrics = yardstick::metric_set(yardstick::rmse)
+    )
+
+  best_learner_name <- fit_learners %>%
+    workflowsets::rank_results(rank_metric = 'rmse') %>%
+    dplyr::select(wflow_id, model, .config, rmse=mean, rank) %>%
+    dplyr::filter(row_number() == 1) %>%
+    dplyr::pull(wflow_id)
+  if (verbose){
+    print(best_learner_name)
+  }
+
+  best_params <- fit_learners %>%
+    workflowsets::extract_workflow_set_result(best_learner_name) %>%
+    tune::select_best(metric='rmse')
+
+  fit_learners %>%
+    tune::extract_workflow(best_learner_name) %>%
+    tune::finalize_workflow(best_params)
+}
+
+
