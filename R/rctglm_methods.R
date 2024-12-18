55d636aed9152f5e63d2b4f928257d8e1374214d
Mathias Lerbech Jeppesen <mathiasljeppesen@outlook.com>
2024-12-17 21:32:29 -0800

    feat: rctglm print method had some whitespace removed

--- R/rctglm_methods.R
+++ R/rctglm_methods.R
@@ -4,19 +4,19 @@ print.rctglm <- function(x, ...) {
   cat("Object of class 'rctglm'\n\n")
   cat("Call:  ",
       paste(deparse(x$call), sep = "\n", collapse = "\n"), "\n\n", sep = "")
-  cat("  - Counterfactual control mean (Psi_0=E[Y|X, A=0]) estimate: ",
+  cat("- Counterfactual control mean (Psi_0=E[Y|X, A=0]) estimate: ",
       x$counterfactual_mean0,
       "\n",
       sep = "")
-  cat("  - Counterfactual control mean (Psi_1=E[Y|X, A=1]) estimate: ",
+  cat("- Counterfactual control mean (Psi_1=E[Y|X, A=1]) estimate: ",
       x$counterfactual_mean1,
       "\n",
       sep = "")
-  cat("  - Estimand function r: ",
+  cat("- Estimand function r: ",
       deparse_fun_body(x$estimand_fun),
       "\n",
       sep = "")
-  cat("  - Estimand (r(Psi_1, Psi_0)) estimate (SE): ",
+  cat("- Estimand (r(Psi_1, Psi_0)) estimate (SE): ",
       x$estimand,
       " (",
       x$se_estimand,
