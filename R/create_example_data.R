7bc19f20425fed5a0293e3659a6be5746b0bf742
Mathias Lerbech Jeppesen <mathiasljeppesen@outlook.com>
2024-12-20 16:25:03 -0800

    feat: create_glm_data function added to generate data for examples, vignettes, tests, etc.

--- R/create_example_data.R
+++ R/create_example_data.R
@@ -1,112 +1,141 @@
-#' create_glm_data <- function(formula_eta, ..., family = gaussian(),
-#'                             extra_args_rfun = list(sd = 1)) {
-#'   family = check_family(family)
+#' Generate data simulated from a GLM
 #'
-#'   data_cols <- list(...)
-#'   data <- as.data.frame(data_cols)
-#'   n <- nrow(data)
+#' Provide a formula, variables and a family to generate a linear predictor
+#' using the formula and provided variables before using the inverse link of
+#' the family to generate the GLM modelled mean, mu, which is then used to
+#' simulate the response with this mean from the generating function according
+#' to the chosen family.
 #'
-#'   cols_env <- rlang::new_environment(data_cols)
-#'   linear_predictor <- withr::with_environment(
-#'     env = cols_env,
-#'     code = {
-#'       ifelse(is.character(formula_eta),
-#'              eval(parse(text = formula_eta)),
-#'              formula_eta)
-#'     }
-#'   )
-#'   mu <- family$linkinv(linear_predictor)
+#' @param formula_eta a [formula] or a `character` specifying how
+#' to generate the linear predictor, eta
+#' @param ... a `data.frame` with columns corresponding to variables used
+#' in `formula_eta`, a named `list` of those variables, or individually provided
+#' named arguments of variables
+#' from
+#' @param family the `family` of the response. this can be a `character`
+#' string naming a family function, a family `function` or the result of
+#' a `call` to a family function
+#' @param family_args a named `list` with values of arguments passed to
+#' family relevant r<family_name> function for simulating the data
 #'
-#'   args_to_rfun <- c(list(family = family), extra_args_rfun)
-#'   rfun <- do.call(family_to_rfun, args_to_rfun)
-#'   deparse_add_arg(rfun)
-#'   y <- rfun(n, mu)
+#' @returns a `data.frame`
+#' @export
 #'
-#'   out <- data %>%
-#'     dplyr::mutate(y = y, .before = everything())
+#' @examples
+#' # Generate a gaussian response from a single covariate
+#' create_glm_data(1+2*x1,
+#'                 x1 = rnorm(10))
 #'
-#'   return(out)
-#' }
+#' # Generate a gaussian response from a single covariate with
+#' # non-linear effects
+#' create_glm_data(1+2*abs(sin(x1)),
+#'                 x1 = runif(min = -2, max = 2))
 #'
-#' family_to_rfun <- function(family, ...) {
-#'   family <- check_family(family)
-#'   family_name_only_letters <- gsub("[^a-zA-Z\\.]", "", family$family)
+#' # Generate a negative binomial response
+#' create_glm_data(1+2*x1-x2,
+#'                 x1 = rnorm(10),
+#'                 x2 = rgamma(10, shape = 2),
+#'                 family = MASS::negative.binomial(2))
 #'
-#'   family_with_dist_as_class <- structure(
-#'     family,
-#'     class = c(
-#'       family_name_only_letters,
-#'       class(family)
-#'     )
-#'   )
-#'
-#'   UseMethod("family_to_rfun", object = family_with_dist_as_class)
-#' }
-#'
-#' deparse_add_arg <- function(fun) {
-#'
-#'   extra_args <- parent.frame()$extra_args_rfun
-#'
-#'   copy <- fun
-#'   body_copy <- body(copy)
-#'
-#'   browser()
-#'   cur_env <- rlang::current_env()
-#'   body(copy) <- substitute(
-#'     body_copy,
-#'     c(cur_env, list(sd = extra_args$sd))
-#'   )
-#'
-#' }
-#'
-#' #' @export
-#' family_to_rfun.gaussian <- function(family, sd = 1, ...) {
-#'   # # eval(
-#'   # #   parse(
-#'   # #     text = paste0(
-#'   # #       "function(n, mu) rnorm(n = n, mean = mu, sd = ", sd, ")"
-#'   # #     )
-#'   # #   )
-#'   # # )
-#'   #
-#'
-#'   fun <- function(n, mu) rnorm(n = n, mean = mu, sd = sd)
-#'   deparse_add_arg(fun)
-#'
-#'   out <- function(n, mu) NULL
-#'
-#'   # Substitute the placeholder with the actual `sd` value
-#'   body(out) <- substitute(
-#'     rnorm(n = n, mean = mu, sd = sd_placeholder),
-#'     list(sd_placeholder = sd)
-#'   )
-#'
-#'   out
-#'
-#'   function(n, mu) rnorm(n = n, mean = mu, sd = sd)
-#' }
-#'
-#' #' @export
-#' family_to_rfun.binomial <- function(family, ...) {
-#'   function(n, mu) rbinom(n = n, size = 1, prob = mu)
-#' }
-#'
-#' #' @export
-#' family_to_rfun.poisson <- function(family, ...) {
-#'   function(n, mu) rpois(n = n, lambda = mu)
-#' }
-#'
-#' check_family <- function(family) {
-#'   if (is.character(family)) {
-#'     family <- get(family, mode = "function", envir = parent.frame())
-#'   }
-#'   if (is.function(family)) {
-#'     family <- family()
-#'   }
-#'   if (is.null(family$family)) {
-#'     print(family)
-#'     stop("'family' not recognized")
-#'   }
-#'
-#'   return(family)
-#' }
+#' # Provide variables as a list/data.frame
+#' create_glm_data(1+2*x1-x2,
+#'                 data.frame(
+#'                   x1 = rnorm(10),
+#'                   x2 = rgamma(10, shape = 2)
+#'                 ),
+#'                 family = MASS::negative.binomial(2))
+create_glm_data <- function(formula_eta,
+                            ...,
+                            family = gaussian(),
+                            family_args = list(sd = 1)) {
+  family = check_family(family)
+
+  data_list <- list(...)
+  if (length(data_list) == 0) cli::cli_abort("You need to specify columns to generate data from")
+  # if (!(length(data) == 1 && inherits(data[[1]], "data.frame")))
+  data <- as.data.frame(data_list)
+
+  n <- nrow(data)
+
+  cols_env <- rlang::new_environment(data)
+  linear_predictor <- withr::with_environment(
+    env = cols_env,
+    code = {
+      ifelse(is.character(formula_eta),
+             eval(parse(text = formula_eta)),
+             formula_eta)
+    }
+  )
+  mu <- family$linkinv(linear_predictor)
+
+  args_to_rfun <- c(list(n = n,
+                         mu = mu,
+                         family = family),
+                    family_args)
+  y <- do.call(generate_from_family, args_to_rfun)
+
+  out <- data %>%
+    dplyr::mutate(y = y, .before = everything())
+
+  return(out)
+}
+
+#' Use the r<family_name> function to generate data from family
+#' @noRd
+generate_from_family <- function(family, n, mu, ...) {
+  family <- check_family(family)
+  family_name_only_small_letters <- tolower(
+    gsub("[^a-zA-Z\\.]", "", family$family)
+  )
+
+  family_with_dist_as_class <- structure(
+    family,
+    class = c(
+      family_name_only_small_letters,
+      class(family)
+    )
+  )
+
+  UseMethod("generate_from_family", object = family_with_dist_as_class)
+}
+
+#' @export
+#' @noRd
+generate_from_family.gaussian <- function(family, n, mu, sd = 1, ...) {
+  rnorm(n = n, mean = mu, sd = sd)
+}
+
+#' @export
+#' @noRd
+generate_from_family.binomial <- function(family, n, mu, ...) {
+  rbinom(n = n, size = 1, prob = mu)
+}
+
+#' @export
+#' @noRd
+generate_from_family.poisson <- function(family, n, mu, ...) {
+  rpois(n = n, lambda = mu)
+}
+
+#' @export
+#' @noRd
+generate_from_family.negativebinomial <- function(family, n, mu, ...) {
+  rnbinom(n = n, size = 1, mu = mu)
+}
+
+#' Transform character or family function to a call
+#' @noRd
+check_family <- function(family) {
+  if (is.character(family)) {
+    family <- get(family, mode = "function", envir = parent.frame())
+  }
+  if (is.function(family)) {
+    family <- family()
+  }
+  if (is.null(family$family)) {
+    print(family)
+    stop("'family' not recognized")
+  }
+
+  return(family)
+}
