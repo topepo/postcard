6302870bc1d4fb00153ae5c3e9e83527a7b202af
Mathias Lerbech Jeppesen <mathiasljeppesen@outlook.com>
2025-02-20 22:09:42 +1300

    ci: Another try

--- tests/testthat/test-utils.R
+++ tests/testthat/test-utils.R
@@ -1,44 +1,45 @@
-# # get_fun_args
-# test_that("`get_fun_args` gives argument names as vector", {
-#   extpkg_args <- get_fun_args(base::t)
-#   thispkg_args <- get_fun_args(get01args)
-#   f <- function(x, y) x+y
-#   predef_args <- get_fun_args(f)
-#   anon_args <- get_fun_args(\(a, b) a-b)
-#
-#   expect_equal(extpkg_args, "x")
-#   expect_equal(thispkg_args, "fun")
-#   expect_equal(predef_args, c("x", "y"))
-#   expect_equal(anon_args, c("a", "b"))
-# })
+# NOT ISSUE
+# get_fun_args
+test_that("`get_fun_args` gives argument names as vector", {
+  extpkg_args <- get_fun_args(base::t)
+  thispkg_args <- get_fun_args(get01args)
+  f <- function(x, y) x+y
+  predef_args <- get_fun_args(f)
+  anon_args <- get_fun_args(\(a, b) a-b)
 
-# deparse_fun_body
-test_that("`deparse_fun_body` produces character string of function body", {
-  extpkg_body <- deparse_fun_body(base::t)
-  thispkg_body <- deparse_fun_body(get01args)
-  f <- function(x, y) {
-    z <- x+y
-    return(z)
-  }
-  predef_body <- deparse_fun_body(f)
-  anon_body <- deparse_fun_body(\(a, b) a-b)
-
-  expect_type(extpkg_body, "character")
-  expect_length(extpkg_body, 1)
-  expect_snapshot(extpkg_body)
-
-  expect_type(thispkg_body, "character")
-  expect_length(thispkg_body, 1)
-  expect_snapshot(thispkg_body)
-
-  expect_type(predef_body, "character")
-  expect_length(predef_body, 1)
-  expect_snapshot(predef_body)
-
-  expect_type(anon_body, "character")
-  expect_length(anon_body, 1)
-  expect_equal(anon_body, "a - b")
+  expect_equal(extpkg_args, "x")
+  expect_equal(thispkg_args, "fun")
+  expect_equal(predef_args, c("x", "y"))
+  expect_equal(anon_args, c("a", "b"))
 })
+
+# # deparse_fun_body
+# test_that("`deparse_fun_body` produces character string of function body", {
+#   extpkg_body <- deparse_fun_body(base::t)
+#   thispkg_body <- deparse_fun_body(get01args)
+#   f <- function(x, y) {
+#     z <- x+y
+#     return(z)
+#   }
+#   predef_body <- deparse_fun_body(f)
+#   anon_body <- deparse_fun_body(\(a, b) a-b)
+#
+#   expect_type(extpkg_body, "character")
+#   expect_length(extpkg_body, 1)
+#   expect_snapshot(extpkg_body)
+#
+#   expect_type(thispkg_body, "character")
+#   expect_length(thispkg_body, 1)
+#   expect_snapshot(thispkg_body)
+#
+#   expect_type(predef_body, "character")
+#   expect_length(predef_body, 1)
+#   expect_snapshot(predef_body)
+#
+#   expect_type(anon_body, "character")
+#   expect_length(anon_body, 1)
+#   expect_equal(anon_body, "a - b")
+# })
 
 # NOT ISSUE
 # get_response_from_formula
